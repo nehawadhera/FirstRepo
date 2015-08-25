4020449485eb22e299cb6d80fb76d08af6a285dd
unknown <wadhera@WADHERA4.americas.hpqcorp.net>
2015-08-25 15:55:32 -0700

    First Git project

---
+++ src/HelloWorld/HelloWorld.java
@@ -0,0 +1,21 @@
+
+package HelloWorld;
+
+public class HelloWorld {
+	
+	public HelloWorld(String name)
+	{
+		System.out.print("Name passed is" +name);
+	}
+	public static void main(String[] args) {
+		HelloWorld hw= new HelloWorld("neha");
+		// TODO Auto-generated method stub
+		
+		int number=3;
+		System.out.println(number);
+				
+			;
+
+	}
+
+}
